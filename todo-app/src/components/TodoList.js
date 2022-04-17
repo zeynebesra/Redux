4079390c30693f8aa0c4166e79737547f41a8c39
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-04-17 22:24:17 +0300

    17.4

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,26 +1,34 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
+// TodoList
 function TodoList() {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
-  console.log(items);
+  //console.log(items);
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
 
   return (
     <ul className="todo-list">
-      {/*<li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-  </li>*/}
-
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -21,7 +21,22 @@ export const todosSlice = createSlice({
       },
     ],
   },
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed;
+    },
+    destroy: (state, action) => {
+      const id = action.payload;
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
+    },
+  },
 });
 
+export const { addTodo, toggle, destroy } = todosSlice.actions;
 export default todosSlice.reducer;
