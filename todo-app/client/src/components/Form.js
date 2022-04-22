eb529bdb474df1b551d51c4fa236159ce9099035
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-04-23 00:27:23 +0300

    changed

--- todo-app/client/src/components/Form.js
+++ todo-app/client/src/components/Form.js
@@ -1,26 +1,41 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
-import { addTodo } from "../redux/todos/todosSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodoAsync } from "../redux/todos/todosSlice";
+import Loading from "./Loading";
 
 function Form() {
   const [title, setTitle] = useState("");
   const dispatch = useDispatch();
-  const handleSubmit = (e) => {
+  const isLoading = useSelector((state) => state.todos.addNewTodoLoading);
+  const error = useSelector((state) => state.todos.addNewTodoError);
+
+  const handleSubmit = async (e) => {
     if (!title) return;
     e.preventDefault();
-    dispatch(addTodo({ title }));
+    await dispatch(addTodoAsync({ title }));
     setTitle("");
   };
+
+  if (error) {
+    alert(error);
+    return;
+  }
+  //
+  //
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", alignItems: "center" }}
+    >
       <input
+        disabled={isLoading}
         className="new-todo"
         placeholder="What needs to be done?"
         autoFocus
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+      {isLoading && <Loading />}
     </form>
   );
 }
--- todo-app/client/src/redux/todos/todosSlice.js
+++ todo-app/client/src/redux/todos/todosSlice.js
@@ -1,9 +1,23 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+//api
+
 export const getTodosAsync = createAsyncThunk(
   "todos/getTodosAsync",
   async () => {
-    const res = await axios("http://localhost:7000/todos");
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`);
+    return res.data;
+  }
+);
+
+export const addTodoAsync = createAsyncThunk(
+  "todos/addTodoAsync",
+  async (data) => {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`,
+      data
+    );
     return res.data;
   }
 );
@@ -15,6 +29,8 @@ export const todosSlice = createSlice({
     isLoading: false,
     error: null,
     activeFilter: "all",
+    addNewTodoLoading: false,
+    addNewTodoError: null,
   },
   reducers: {
     addTodo: {
@@ -50,6 +66,7 @@ export const todosSlice = createSlice({
     },
   },
   extraReducers: {
+    //get todos
     [getTodosAsync.pending]: (state, action) => {
       state.isLoading = true;
     },
@@ -57,9 +74,23 @@ export const todosSlice = createSlice({
       state.items = action.payload;
       state.isLoading = false;
     },
+    //hata durumu
     [getTodosAsync.rejected]: (state, action) => {
       state.isLoading = false;
       state.error = action.error.message;
+    },
+
+    //add todo
+    [addTodoAsync.pending]: (state, action) => {
+      state.addNewTodoLoading = true;
+    },
+    [addTodoAsync.fulfilled]: (state, action) => {
+      state.items.push(action.payload);
+      state.addNewTodoLoading = false;
+    },
+    [addTodoAsync.rejected]: (state, action) => {
+      state.addNewTodoLoading = false;
+      state.addNewTodoError = action.error.message;
     },
   },
 });
